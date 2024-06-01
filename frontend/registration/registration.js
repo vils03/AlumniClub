3c4c4827d1a35f03e7c71584f4c3a2050f17785b
vils03 <velislava2704@gmail.com>
2024-06-01 15:57:41 +0200

    Registration userTypes fixed v.3

--- frontend/registration/registration.js
+++ frontend/registration/registration.js
@@ -1,29 +1,31 @@
 // Display additional section
-function showAdditionalSection(event, sectionId) {
+function showAdditionalSection(event, sectionId, userType) {
     const recruiter = document.getElementById('recruiter');
     const graduate = document.getElementById('graduate');
     const additionalSection = document.getElementById(sectionId);
     event.preventDefault();
     var otherSection;
     if(sectionId == 'recruiter-info'){
+        state.userType = 'recruiter';
         otherSection = document.getElementById('graduate-info');
     }
     else{
+        state.userType = 'graduate';
         otherSection = document.getElementById('recruiter-info');
     }
     recruiter.parentNode.remove();
     graduate.parentNode.remove();
     otherSection.style.display = 'none';
     additionalSection.style.display = 'block';
-    
 }
 
 const recruiter = document.getElementById('recruiter');
 const graduate = document.getElementById('graduate');
 
-recruiter.addEventListener('click', (event) => showAdditionalSection(event, 'recruiter-info'));
-graduate.addEventListener('click', (event) => showAdditionalSection(event, 'graduate-info'));
+const state = { userType: null };
 
+recruiter.addEventListener('click', (event) => showAdditionalSection(event, 'recruiter-info', state));
+graduate.addEventListener('click', (event) => showAdditionalSection(event, 'graduate-info', state));
 // Get info from HTML form 
 const registrationForm = document.getElementById('register-form');
 registrationForm.addEventListener('submit', (event) => {
@@ -31,6 +33,7 @@ registrationForm.addEventListener('submit', (event) => {
     const inputs = registrationForm.querySelectorAll('input, select');
 
     const userData = {};
+    userData['type'] = state['userType'];
     inputs.forEach(input => {
         userData[input.id] = input.value;
     });
