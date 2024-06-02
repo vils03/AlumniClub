62452d3c7b1c87c4a5633695214286a901d8f801
vils03 <velislava2704@gmail.com>
2024-06-02 14:40:59 +0200

    Comments from db script removed

--- database/alumni_db.sql
+++ database/alumni_db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE AlumniClub;
 
-CREATE TABLE Users ( -- Represents the main user type.
+CREATE TABLE Users (
     UserId          INT      NOT NULL,
     FirstName       VARCHAR(30) NOT NULL,
     LastName        VARCHAR(30) NOT NULL,
@@ -9,16 +9,11 @@ CREATE TABLE Users ( -- Represents the main user type.
     PhoneNumber     VARCHAR(30) NOT NULL,
     UserType        VARCHAR(30) NOT NULL,
      
-    --
     CONSTRAINT User_PK  PRIMARY KEY (UserId),
-    --CONSTRAINT Person_AK1 UNIQUE ( -- Composite ALTERNATE KEY.
-    --    FirstName,
-    --    LastName,
-    --),
-    CONSTRAINT User_AK3 UNIQUE (EmailAddress)    -- ALTERNATE KEY.
+    CONSTRAINT User_AK3 UNIQUE (EmailAddress)
 );
 
-CREATE TABLE Major ( -- FMI COURSES.
+CREATE TABLE Major (
     MajorId         INT      NOT NULL,
     MajorName            CHAR(30) NOT NULL,  
     -- 
@@ -26,15 +21,15 @@ CREATE TABLE Major ( -- FMI COURSES.
     CONSTRAINT Major_AK UNIQUE      (MajorName)   
 );
 
-CREATE TABLE Graduate ( -- Stands for one of the subtypes.
-    GraduateId         INT      NOT NULL, -- Must be constrained as (a) the PRIMARY KEY and (b) a FOREIGN KEY.
-    FN                 VARCHAR(30) NOT NULL, --
+CREATE TABLE Graduate (
+    GraduateId         INT      NOT NULL,
+    FN                 VARCHAR(30) NOT NULL,
 	Major              VARCHAR(30) NOT NULL,
 	Class              INT      NOT NULL,
 	Status             BIT NOT NULL,
 	Location           VARCHAR(100) NOT NULL,
     MajorId            INT      NOT NULL,
-    -- 
+
     CONSTRAINT Graduate_PK            PRIMARY KEY (GraduateId),
     CONSTRAINT GraduateToUser_FK      FOREIGN KEY (GraduateId)
         REFERENCES Users (UserId),
@@ -42,10 +37,10 @@ CREATE TABLE Graduate ( -- Stands for one of the subtypes.
         REFERENCES Major (MajorId)   
 );
 
-CREATE TABLE Recruiter ( -- Denotes one of the subtypes
-    RecruiterId      INT         NOT NULL, -- Must be constrained as (a) the PRIMARY KEY and (b) a FOREIGN KEY.
+CREATE TABLE Recruiter (
+    RecruiterId      INT         NOT NULL,
     CompanyName      VARCHAR(50) NOT NULL,  
-    -- 
+
     CONSTRAINT Recruiter_PK         PRIMARY KEY (RecruiterId),
     CONSTRAINT RecruiterToUser_FK  FOREIGN KEY (RecruiterId)
         REFERENCES Users (UserId)   
@@ -56,16 +51,16 @@ CREATE TABLE EventInfo  (
     EventName       VARCHAR(30)  NOT NULL,
     EventDesc       VARCHAR(200) NOT NULL,  
     CreatedEventDateTime DATETIME NOT NULL,
-    -- 
-    CONSTRAINT Country_PK PRIMARY KEY (EventId),
+
+    CONSTRAINT Country_PK PRIMARY KEY (EventId)
 	   
 );
 
-CREATE TABLE UECreated ( -- cross table between users and events created
+CREATE TABLE UECreated (
 	UECreatedId INT NOT NULL,
 	UserId INT NOT NULL,
 	EventId INT NOT NULL,
-	-- 
+
 	CONSTRAINT UECreated_PK        PRIMARY KEY (UECreatedId),
     CONSTRAINT UECreatedToUser_FK  FOREIGN KEY (UserId)
         REFERENCES Users (UserId),
@@ -73,11 +68,11 @@ CREATE TABLE UECreated ( -- cross table between users and events created
         REFERENCES EventInfo (EventId)
 );
 
-CREATE TABLE UEAccepted ( -- cross table between users and events accepted
+CREATE TABLE UEAccepted (
 	UEAcceptedId INT NOT NULL,
 	UserId     INT NOT NULL,
 	EventId    INT NOT NULL,
-	-- 
+
 	CONSTRAINT UEAccepted_PK        PRIMARY KEY (UEAcceptedId),
     CONSTRAINT UEAcceptedToUser_FK  FOREIGN KEY (UserId)
         REFERENCES Users (UserId),
@@ -85,12 +80,12 @@ CREATE TABLE UEAccepted ( -- cross table between users and events accepted
         REFERENCES EventInfo (EventId)
 );
 
-CREATE TABLE MessagesInfo ( -- table for messages
+CREATE TABLE MessagesInfo (
 	MessagesInfoId INT NOT NULL,
 	SenderId       INT NOT NULL,
 	RecipientId    INT NOT NULL,
 	MessageText    VARCHAR(100) NOT NULL,
-	-- 
+	
 	CONSTRAINT MessagesInfo_PK        PRIMARY KEY (MessagesInfoId),
     CONSTRAINT MessagesInfoToSender_FK  FOREIGN KEY (SenderId)
         REFERENCES Users (UserId),
@@ -98,13 +93,13 @@ CREATE TABLE MessagesInfo ( -- table for messages
         REFERENCES Users (UserId)
 );
 
-CREATE TABLE AdInfo ( -- table for ads from recruiter
+CREATE TABLE AdInfo (
 	AdId              INT NOT NULL,
 	RecruiterId       INT NOT NULL,
 	AdName            VARCHAR(50) NOT NULL,
 	AdDesc            VARCHAR(200) NOT NULL,
     CreatedEventDateTime DATETIME NOT NULL,
-	-- 
+	
 	CONSTRAINT AdInfo_PK        PRIMARY KEY (AdId),
     CONSTRAINT AdInfoToRecruiter_FK  FOREIGN KEY (RecruiterId)
         REFERENCES Users (UserId)
