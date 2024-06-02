821ec665a44b03909727bc997af3b307c9783f5d
vils03 <velislava2704@gmail.com>
2024-06-02 15:33:35 +0200

    ÄPI for registration added

---
+++ backend/api/registration.php
@@ -0,0 +1,25 @@
+<?php
+
+$phpInput = json_decode(file_get_contents('php://input'), true);
+require_once "../models/User.php";
+
+if($phpInput['type'] == 'graduate'){
+    $user = new Graduate(null, $phpInput['name'], $phpInput["last-name"], $phpInput['email'], $phpInput['password'], 
+                            $phpInput['phone-numer'], $phpInput['type'], $phpInput['fn'], $phpInput['major'], 
+                            $phpInput['class'], $phpInput['status'], $phpInput['location']);
+}
+else{
+    $user = new Recruiter(null, $phpInput['name'], $phpInput["last-name"], $phpInput['email'], $phpInput['password'], 
+    $phpInput['phone-numer'], $phpInput['type'], $phpInput['company']);
+}
+
+try {
+    $user->validate();
+    $user->storeInDB();
+    echo json_encode(['success' => true]);
+} catch (Exception $e) {
+    echo json_encode([
+        'success' => false,
+        'message' => $e->getMessage(),
+    ]);
+}    
