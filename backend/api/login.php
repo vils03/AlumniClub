1c582d4b45aad0be9649de5ba8027fbc49046887
vils03 <velislava2704@gmail.com>
2024-06-06 17:25:25 +0200

    API's added

--- backend/api/login.php
+++ backend/api/login.php
@@ -1,15 +1,8 @@
 <?php
-// Set session configuration options
-ini_set('session.cookie_lifetime', 0); // Session cookie lifetime, 0 means until the browser is closed
-ini_set('session.use_strict_mode', 1); // Use strict mode to prevent session fixation
-ini_set('session.use_cookies', 1); // Use cookies to store session ID
-ini_set('session.use_only_cookies', 1); // Don't use URL-based session IDs
 session_start();
 $phpInput = json_decode(file_get_contents('php://input'), true);
 require_once "../models/User.php";
 mb_internal_encoding("UTF-8");
-
-
 
 if (empty($phpInput['email'])){ 
     echo json_encode([
